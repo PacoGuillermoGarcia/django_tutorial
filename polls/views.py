04fe714cef1882ec3bd37899da2b7f77e21c996d
PacoGuillermoGarcia <pakotoes3@gmail.com>
2020-03-03 09:20:32 +0100

    añadir fichero .travis.yml

--- polls/views.py
+++ polls/views.py
@@ -9,7 +9,7 @@ from .models import Choice, Question
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
+    context_object_name = 'latest_question_list_1'
 
     def get_queryset(self):
         """
